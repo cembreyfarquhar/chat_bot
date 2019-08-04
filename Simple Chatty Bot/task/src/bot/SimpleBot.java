a09e0711a5adf53bbae7ca7146fef2350dc107d1
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-03 21:04:35 -0500

    Bot can read input for name and age remainders, and calculate age based on an equation using the age remainders

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,8 +1,38 @@
 package bot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Sage.");
         System.out.println("I was created in 2019.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        String input = scanner.nextLine();
+
+        String[] inputArr = input.split(" ");
+
+        int[] remainderArr = new int[3];
+
+        for (int i = 0; i < remainderArr.length; i++) {
+            remainderArr[i] = Integer.parseInt(inputArr[i]);
+        }
+
+        // reading all remainders
+
+        int yourAge = (remainderArr[0] * 70 + remainderArr[1] * 21 + remainderArr[2] * 15) % 105;
+
+        System.out.println("Your age is " + yourAge + " that's a good time to start programming!");
     }
 }
---
+++ Simple Chatty Bot/task/src/bot/Testing.java
@@ -0,0 +1,16 @@
+package bot;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("STARTING PROGRAM");
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("ENDING PROGRAM");
+    }
+}
