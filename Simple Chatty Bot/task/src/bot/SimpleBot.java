603b95acb12975ccbce9581989d3bd80d958d7dc
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-04 14:40:25 -0500

    complete!

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -3,33 +3,62 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Aid", "2018"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
 
-        System.out.println("Hello! My name is Sage.");
-        System.out.println("I was created in 2019.");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
 
+    static void remindName() {
         String name = scanner.nextLine();
-
         System.out.println("What a great name you have, " + name + "!");
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+    }
 
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
-
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
 
-        // read a number and count to it here
-        int number = scanner.nextInt();
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        boolean correct = false;
+        System.out.println("What is 15 in binary?");
+        System.out.println("1. 00100101");
+        System.out.println("2. 11100101");
+        System.out.println("3. 01101101");
+        System.out.println("4. 00001111");
+        int answer = scanner.nextInt();
+        while (answer != 4) {
+            System.out.println("Please, try again.");
+            answer = scanner.nextInt();
+        }
+    }
 
-        for (int i = 1; i <= number; i++) System.out.println(i + "!");
-
-        System.out.println("Completed, have a nice day!");
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
 }
