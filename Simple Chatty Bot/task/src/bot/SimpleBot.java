99318a3e4a678b06c4e7331d97d0937254657e53
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-03 22:17:49 -0500

    next

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,7 +1,5 @@
 package bot;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class SimpleBot {
@@ -12,27 +10,23 @@ public class SimpleBot {
         System.out.println("I was created in 2019.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        String yourName = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have, " + yourName + "!");
+        System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        String input = scanner.nextLine();
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        String[] inputArr = input.split(" ");
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        int[] remainderArr = new int[3];
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
 
-        for (int i = 0; i < remainderArr.length; i++) {
-            remainderArr[i] = Integer.parseInt(inputArr[i]);
-        }
+        // read a number and count to it here
 
-        // reading all remainders
-
-        int yourAge = (remainderArr[0] * 70 + remainderArr[1] * 21 + remainderArr[2] * 15) % 105;
-
-        System.out.println("Your age is " + yourAge + " that's a good time to start programming!");
+        System.out.println("Completed, have a nice day!");
     }
 }
